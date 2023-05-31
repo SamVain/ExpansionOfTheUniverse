deafaec758d7520b40d1312108743a72a8d8c2ea
SamVain <sam.vain@hotmail.co.uk>
2023-05-31 23:38:21 +0100

    Second Commit

--- expansion_of_the_universe.py
+++ expansion_of_the_universe.py
@@ -3,40 +3,41 @@ import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 # Constants for a more realistic Universe
-H_0 = 70  # Hubble constant at present time, in km/s/Mpc
 Omega_m = 0.3  # Fractional matter density
 Omega_r = 1e-5  # Fractional radiation density
 Omega_lambda = 0.7  # Fractional dark energy density
 Omega_k = 1 - Omega_m - Omega_r - Omega_lambda  # Fractional curvature density
-G = 6.67430e-11  # Gravitational constant, in m^3 kg^-1 s^-2
-# Critical density at present time, in kg/m^3
-rho_crit_0 = 3 * H_0**2 / (8 * np.pi * G)
-rho_m_0 = Omega_m * rho_crit_0  # Matter density at present time, in kg/m^3
-rho_r_0 = Omega_r * rho_crit_0  # Radiation density at present time, in kg/m^3
 
-# Friedmann equation: (da/dt)^2 = 8*pi*G/3 * (rho_m_0*(a_0/a)^3 + rho_r_0*(a_0/a)^4) + Omega_lambda*H_0**2*a_0**2 - Omega_k*H_0**2*a_0**2*(a_0/a)^2
+# Friedmann equation: (da/dt)^2 = Omega_m/a + Omega_r/a^2 + Omega_lambda*a^2 - Omega_k
 # We will solve this equation numerically to find a(t)
 
 # First, we create a function for the derivative in the equation
-
-
 def da_dt(t, a):
-    return np.sqrt(8 * np.pi * G / 3 * (rho_m_0 / a**3 + rho_r_0 / a**4) + Omega_lambda * H_0**2 * a**2 - Omega_k * H_0**2 * a)
-
-
-# Time span
-t_span = [0, 1e17]  # Time since Big Bang, in seconds
+    return np.sqrt(Omega_m / a + Omega_r / a**2 + Omega_lambda * a**2 - Omega_k)
 
 # Initial conditions
-a_0 = 1
-initial_conditions = [a_0]
+a_0_past_present = 1e-10  # The Universe was much smaller in the past
+a_0_future = 1  # At present time, the Universe is of size 1
 
-# Solve the differential equation using the Runge-Kutta method
-solution = solve_ivp(da_dt, t_span, initial_conditions, method='RK45')
+# Time span for the past and present
+t_span_past_present = [0, 1]  # From Big Bang to present time
 
-# Plot the expansion of the Universe
-plt.plot(solution.t, solution.y[0])
-plt.xlabel('Time since Big Bang (s)')
-plt.ylabel('Scale factor a(t)')
+# Solve the differential equation for the past and present
+solution_past_present = solve_ivp(da_dt, t_span_past_present, [a_0_past_present], method='RK45')
+
+# Time span for the future
+t_span_future = [1, 2]  # From present time to twice the current age of the universe
+
+# Solve the differential equation for the future
+solution_future = solve_ivp(da_dt, t_span_future, [a_0_future], method='RK45')
+
+# Plot the expansion of the Universe from Big Bang to present
+plt.plot(solution_past_present.t, solution_past_present.y[0], label='Past and present')
+# Plot the predicted future expansion of the Universe
+plt.plot(solution_future.t, solution_future.y[0], linestyle='dotted', label='Predicted future')
+
+plt.xlabel('Time (in units of current age of the Universe)')
+plt.ylabel('Scale factor a(t) (in units of current size of the Universe)')
 plt.title('Expansion of the Universe')
+plt.legend()
 plt.show()
